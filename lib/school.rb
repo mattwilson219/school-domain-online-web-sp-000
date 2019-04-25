fdcc0d22d026fcaa87178ced5a9fd30624bee0fb
Matthew Wilson <wilsonmatt219@gmail.com>
2019-04-25 18:49:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,13 @@ class School
     @roster = {}
   end
 
+  def add_student(name, grade)
+        if roster.has_key?(grade)
+          roster[grade] << name
+        else roster[grade] = [name]
+    end 
+  end
+
 
 
 end
